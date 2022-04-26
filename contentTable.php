dc75c3f1b1a968c601f3be852e11aee5c8e6c2fa
Dimitris Margelos <jimmarg321@gmail.com>
2022-04-26 16:47:13 +0300

    Full functionality

--- contentTable.php
+++ contentTable.php
@@ -11,36 +11,60 @@
 
 function enqueue_content_table_scripts_and_styles(){
 	wp_enqueue_style('content-table-styles', plugins_url('assets/css/content-table.css', __FILE__));
-	wp_enqueue_script('content-table-script', plugins_url( 'assets/js/content-table.js' , __FILE__ ), array(), '1.0.0', true);
+	wp_enqueue_script('content-table-script', plugins_url( 'assets/js/content-table.js' , __FILE__ ), array(), '1.0.0', false);
 }
 
 add_action('wp_enqueue_scripts','enqueue_content_table_scripts_and_styles');
 
 // Shortcode function for sceenshot button
-function content_table( $atts ) {
+function content_table_function() {
 	ob_start();
- 
-    // define attributes/parameters and their defaults
-
-    $attributes = shortcode_atts(
-        array(
-            'title' => 'Page Screenshot',
-            'color' => '',
-         ), 
-        $atts
-    );
-    
-    $postid = get_the_ID();
 
     ?>
-        <div class="container-scrshot-your-page text-center">
-            <div class="row" id="post-<?php echo $postid; ?>">
+        <div class="container-content-table text-center">
+            <div class="row" id="ct_TableOfContents">
                 <div class="col-sm-6 col-md-3">
-                    <button id="button-<?php echo $postid; ?>" class="scrshot-btn" style="background-color: <?php echo $attributes['color'] ?>"><?php echo $attributes['title'] ?></button>
+                    <p class="ct_content-table_title">Περιεχόμενα</p>
+                    <ul class="ct_list-of-content"></ul>
                 </div>
             </div>
+
+            <script>
+                window.onload = get_headings;
+                
+                function get_headings() {
+                    const h = ["h2", "h3", "h4", "h5", "h6"];
+                let contentOfHeaders = [];
+                let allHeaders = [];
+                let i = z = 1;
+
+                for (let i = 0; i < h.length; i++) {
+                    allHeaders[i] = document.querySelectorAll(h[i]);
+                }
+
+                for (let j = 0; j < allHeaders.length; j++) {
+                    for (let y = 0; y < allHeaders[j].length; y++) {
+                        allHeaders[j][y].id = "heading_" + z;
+                        contentOfHeaders.push(allHeaders[j][y].textContent);
+                        z++;
+                    }
+                }
+                console.log(allHeaders);
+                contentOfHeaders.forEach(function(item) {
+    
+                    let list = document.createElement("li");
+                    let listLink = document.createElement("a");
+                    listLink.innerText = item;
+                    listLink.href = "#heading_" + i;
+                    list.appendChild(listLink);
+                        document.querySelector('.ct_list-of-content').appendChild(list);
+                    i++;
+                })
+                }
+
+            </script>
     <?php
         $myvariable = ob_get_clean();
         return $myvariable;
 }
-add_shortcode( 'content', 'content_table' );
+add_shortcode( 'content_table', 'content_table_function' );
